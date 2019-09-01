27636c6367e7b624ba02487d9b7347badd7e9099
mazhar4437 <mazharmanasiya4@gmail.com>
2019-09-01 13:46:05 +0500

    Commit Style

--- FrontEnd/course-app/src/app/app-routing.module.ts
+++ FrontEnd/course-app/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from 'src/app/login/login.component';
+import { AppComponent } from 'src/app/app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',
+    component : LoginComponent
+  },
+  
+  {
+  path:'login',
+  component : LoginComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
