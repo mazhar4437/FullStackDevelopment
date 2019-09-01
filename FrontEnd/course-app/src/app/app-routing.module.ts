d2d91a6b501a74c70fd778fea15ba1833b897002
mazhar4437 <mazharmanasiya4@gmail.com>
2019-09-01 14:08:42 +0500

    Component Commit

--- FrontEnd/course-app/src/app/app-routing.module.ts
+++ FrontEnd/course-app/src/app/app-routing.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from 'src/app/login/login.component';
 import { AppComponent } from 'src/app/app.component';
+import { HomeComponent } from 'src/app/home/home.component';
 
 
 const routes: Routes = [
-  { path:'',
-    component : LoginComponent
-  },
-  
   {
-  path:'login',
-  component : LoginComponent
+    path: '',
+    component: LoginComponent
+  },
+{
+  path: 'Home',
+  component: HomeComponent
+},
+  {
+    path: 'login',
+    component: LoginComponent
   }
-  
+
 ];
 
 @NgModule({
